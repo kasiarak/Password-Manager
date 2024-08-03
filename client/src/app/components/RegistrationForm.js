a20e5082c02f35ad1399e220e2e92295cb72b2c0
Katarzyna Rak <katarzynarak9@gmail.com>
2024-08-04 01:41:47 +0200

    Error handling for empty fields in registration

--- client/src/app/components/RegistrationForm.js
+++ client/src/app/components/RegistrationForm.js
@@ -1,34 +1,72 @@
 import { Poppins } from 'next/font/google';
 import styles from './RegistrationForm.module.css'
+import { useState } from 'react';
 const poppins = Poppins({ subsets: ['latin'], weight: ['500'],  });
 
 function RegistrationForm({ setIsUserRegistered }){
+    const [username, setUsername] = useState(""); 
+    const handleUsername = (event) => setUsername(event.target.value);
+    const [email, setEmail] = useState(""); 
+    const handleEmail = (event) => setEmail(event.target.value);
+    const [password, setPassword] = useState(""); 
+    const handlePassword = (event) => setPassword(event.target.value);
+    const [confirmedPassword, setConfirmedPassword] = useState(""); 
+    const handleConfirmedPassword = (event) => setConfirmedPassword(event.target.value);
+
+    const [message, setMessage] = useState();
+
+    async function registration(){
+        const body = {
+           username: username,
+           email: email,
+           password: password,
+           confirmedPassword: confirmedPassword
+        }
+        try {
+            const response = await fetch(process.env.NEXT_PUBLIC_API_URL + '/registration', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            });
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const data = await response.json(); 
+            setMessage(data.message);
+        } catch (error) {
+            console.error(error);
+            setMessage('Registration failed. Please try again later.');
+        }
+    }
+    
     return(
         <div className={styles.registrationForm}>
         <h2>Registration</h2>
         <div className={styles.registrationInputs}>
             <img src='person-svgrepo-com.svg'/>
-            <input className={poppins.className} placeholder='Username'></input>
+            <input value={username} onChange={handleUsername} className={poppins.className} placeholder='Username'></input>
         </div>
         <hr></hr>
         <div className={styles.registrationInputs}>
             <img id={styles.emailIcon} src='email-svgrepo-com.svg'/>
-            <input type="email" className={poppins.className} placeholder='Email' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></input>
+            <input value={email} onChange={handleEmail} type="email" className={poppins.className} placeholder='Email' pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required></input>
         </div>
         <hr></hr>
         <div className={styles.registrationInputs}>
             <img src='lock-svgrepo-com.svg'/>
-            <input type="password" className={poppins.className} placeholder='Password'></input>
+            <input value={password} onChange={handlePassword} type="password" className={poppins.className} placeholder='Password'></input>
         </div>
         <hr></hr>
         <div className={styles.registrationInputs}>
             <img src='key-svgrepo-com.svg'/>
-            <input type="password" className={poppins.className} placeholder='Confirm Password'></input>
+            <input value={confirmedPassword} onChange={handleConfirmedPassword} type="password" className={poppins.className} placeholder='Confirm Password'></input>
         </div>
         <hr></hr>
         <div className={styles.buttons}>
-            <button className={poppins.className}>Create Account</button>
-            <p className={styles.message}></p>
+            <button onClick={registration} className={poppins.className}>Create Account</button>
+            <p className={styles.message}>{message}</p>
             <h3>Already have an account?</h3>
             <button  onClick={() => setIsUserRegistered(true)} className={poppins.className}>Sing in</button>
         </div>
--- server/server.js
+++ server/server.js
@@ -1,9 +1,52 @@
 import { createServer } from 'http';
-const PORT = process.env.PORT;
+import pool from './db.js';
+
+const PORT = process.env.PORT || 8080;
+
+const jsonMiddleWare = (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next(); 
+}
+const registration = (req, res) => {
+    let body = '';
+    req.on('data', (chunk) => {
+        body += chunk.toString();
+    });
+    req.on('end', () =>{
+        const parsedBody = JSON.parse(body);
+        const { username, email, password, confirmedPassword } = parsedBody;
+        if(parsedBody.username==="" || parsedBody.email==="" 
+            || parsedBody.password==="" || parsedBody.confirmedPassword===""){
+            res.write(JSON.stringify({message: 'All fields must be complete'}));
+            res.end();  
+            return;  
+        }
+    })
+}
+const notFoundHandler = (req,res) => {
+    res.statusCode = 404;
+    res.write(JSON.stringify({message: 'Route not found'}));
+    res.end();
+}
 
 const server = createServer((req, res) =>{
-    res.setHeader('Content-Type', 'application/json');
-    res.end();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+
+    jsonMiddleWare(req, res, () => {
+        if(req.url==='/registration' && req.method==='POST'){
+            registration(req, res);
+        }else{
+            notFoundHandler(req,res); 
+        }
+    });
 });
 
 server.listen(PORT, () => {
