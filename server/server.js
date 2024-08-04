fde6864bbcebd4286c57cce102da4d9fde005da3
Katarzyna Rak <katarzynarak9@gmail.com>
2024-08-04 13:27:03 +0200

    Adding dotenv and error handler for username and emails

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 import { createServer } from 'http';
 import pool from './db.js';
+import dotenv from 'dotenv';
 
-const PORT = process.env.PORT || 8080;
+dotenv.config();
+const PORT = process.env.PORT;
 
 const jsonMiddleWare = (req, res, next) => {
     res.setHeader('Content-Type', 'application/json');
@@ -12,20 +14,29 @@ const registration = (req, res) => {
     req.on('data', (chunk) => {
         body += chunk.toString();
     });
-    req.on('end', () =>{
+    req.on('end', async () =>{
         const parsedBody = JSON.parse(body);
         const { username, email, password, confirmedPassword } = parsedBody;
+        const [users] = await pool.query('SELECT * FROM users');
+        const userUsername = users.find((user) => user.username === parsedBody.username)
+        const userEmail = users.find((user) => user.email === parsedBody.email);
         if(parsedBody.username==="" || parsedBody.email==="" 
             || parsedBody.password==="" || parsedBody.confirmedPassword===""){
             res.write(JSON.stringify({message: 'All fields must be complete'}));
             res.end();  
             return;  
+        }else if(userUsername){
+            res.write(JSON.stringify({message: 'Username already exists'}));
+            res.end(); 
+        }else if(userEmail){
+            res.write(JSON.stringify({message: 'Email already exists'}));
+            res.end(); 
         }
     })
 }
 const notFoundHandler = (req,res) => {
     res.statusCode = 404;
-    res.write(JSON.stringify({message: 'Route not found'}));
+    res.write(JSON.stringify({message: 'Route not foundee'}));
     res.end();
 }
 
