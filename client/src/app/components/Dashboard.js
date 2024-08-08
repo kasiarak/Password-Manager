5a31ac3879ab8f2ec5934a0de281b3549805bc93
Katarzyna Rak <katarzynarak9@gmail.com>
2024-08-08 23:03:20 +0200

    Improve filters button

--- client/src/app/components/Dashboard.js
+++ client/src/app/components/Dashboard.js
@@ -6,7 +6,13 @@ const poppins = Poppins({ subsets: ['latin'], weight: ['500'],  });
 
 function Dashboard({ username }){
     const [savedPasswords, setSavedPasswords] = useState(0);
+    const [filters, setFilters] = useState(['white', 'white', 'white']);
 
+    const filterHandle = (index) => {
+        const newFilters = ['white', 'white', 'white'];
+        newFilters[index] = filters[index] === 'white' ? 'var(--accent-color)' : 'white';
+        setFilters(newFilters);
+    }
     return(
         <div className={styles.dashboard}>
             <div className={styles.dashboardHeader}>
@@ -15,7 +21,14 @@ function Dashboard({ username }){
                 <div className={styles.container}>
                     <div className={styles.buttons}>
                         <div className={`${styles.savedPasswords} ${poppins.className}`}><p>{savedPasswords}<br></br>saved passwords</p></div>
+                        <div className={styles.dropdown}>
                         <div className={`${styles.filters} ${poppins.className}`}><p>Filters</p><img src='settings-setup-svgrepo-com.svg'></img></div>
+                        <div className={styles.dropdownContent}>
+                            <div style={{backgroundColor: filters[0], color: filters[0]==='white' ? 'var(--secondary-color)' : 'white'}} onClick={() => filterHandle(0)} className={styles.filter}>Sort by Date</div>
+                            <div style={{backgroundColor: filters[1], color: filters[1]==='white' ? 'var(--secondary-color)' : 'white'}} onClick={() => filterHandle(1)} className={styles.filter}>Sort by Security Rank</div>
+                            <div style={{backgroundColor: filters[2], color: filters[2]==='white' ? 'var(--secondary-color)' : 'white'}} onClick={() => filterHandle(2)} className={styles.filter}>Sort Alphabetically</div>
+                        </div>
+                        </div>
                     </div>
                     <button className={poppins.className}>Add password</button>
                 </div>
