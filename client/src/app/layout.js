c150674d35ee1942e508608671cbfde6c253ae96
Katarzyna Rak <katarzynarak9@gmail.com>
2024-08-17 03:03:22 +0200

    Set metadata

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,18 +1,19 @@
 import { Poppins } from "next/font/google";
-import Head from "next/head";
 import "./globals.css";
 
-const poppins = Poppins({ subsets: ['latin'], weight: ['500'],  });
+const poppins = Poppins({ subsets: ['latin'], weight: ['500'] });
+
+export const metadata = {
+  title: "Password Manager",
+  description: "Manage your passwords securely",
+  icons: {
+    icon: "/lock-svgrepo-com.svg",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <meta charset="UTF-8"/> 
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <title>Password Manager</title>
-        <link rel = "icon" type="image/svg" href = ""/>
-      </Head>
       <body className={poppins.className}>{children}</body>
     </html>
   );
