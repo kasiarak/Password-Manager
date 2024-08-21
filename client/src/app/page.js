15a5d97ba0f5fa02f700009f59c48778dacce4e7
Katarzyna Rak <katarzynarak9@gmail.com>
2024-08-22 01:17:03 +0200

    Dark mode fix

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -39,7 +39,18 @@ export default function Home() {
   }
 
   useEffect(() => {
-    switchMode();
+    if(localStorage.getItem('mode') === 'dark') {
+      setMode('dark');
+      document.documentElement.style.setProperty('--main-color', '#292828');
+      document.documentElement.style.setProperty('--secondary-color', '#ffffff');
+      document.documentElement.style.setProperty('--scrollbar-color', '#606060 #2b2b2b');
+      document.documentElement.style.setProperty('--img-filter', 'invert(1) brightness(2)');
+    }else{
+      document.documentElement.style.setProperty('--main-color', '#ffffff');
+      document.documentElement.style.setProperty('--secondary-color', '#000000');
+      document.documentElement.style.setProperty('--scrollbar-color', '#c0c0c0 #f0f0f0');
+      document.documentElement.style.setProperty('--img-filter', 'none');
+    }
   },[])
 
   function switchMode(){
